df2131c24067fcfc61c07ee3a821c641723c7053
hyone <hyone.development@gmail.com>
2014-05-03 13:37:38 +0900

    Execute apt-get update before test

--- test/cookbooks/hyone_tmux-test/metadata.rb
+++ test/cookbooks/hyone_tmux-test/metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures tmux-test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'hyone_tmux'
+depends          'apt'
