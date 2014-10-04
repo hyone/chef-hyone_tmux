918e6cd9371f7361aaeab581456b9abbb1fd1df9
hyone <hyone.development@gmail.com>
2014-10-04 17:08:22 +0900

    Support CentOS 7

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,10 +1,18 @@
+
 case node['platform_family']
 when 'rhel', 'fedora'
-  include_recipe 'yum-repoforge'
+  major_version = node['platform_version'].split('.').first.to_i
+  if node['platform'] == 'centos' and major_version < 7
+      include_recipe 'yum-repoforge'
 
-  package 'tmux' do
-    action :install
-    options '--enablerepo=rpmforge'
+      package 'tmux' do
+        action :install
+        options '--enablerepo=rpmforge'
+      end
+  else
+    package 'tmux' do
+      action :install
+    end
   end
 when 'debian'
   package 'tmux' do
